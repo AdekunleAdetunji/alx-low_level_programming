1137ccc4a46428ae804cbc443628d9dfcd981f61
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-16 08:14:07 -0700

    A C source file that prints to stdout using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ * using printf function
+ * Return: Returns integer value 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
