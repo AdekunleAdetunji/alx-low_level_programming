0f1137cf48d5a42955c992070e69a87197471a76
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-16 08:06:02 -0700

    A c file that prints to stdout using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - print using puts() to print out "Programing is like building a
+ * multilingual puzzle
+ */
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
