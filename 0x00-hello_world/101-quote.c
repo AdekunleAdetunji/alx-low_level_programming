34b383810647d04caddde48260599622478dae36
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-16 09:12:11 -0700

    C code that prints output to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - function to print to the standard error
+ * Return: Returns 1 as its output
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, \
+2015-10-19\n");
+	return (1);
+}
