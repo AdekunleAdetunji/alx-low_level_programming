40a3880bde7684dae76430d6b95aefccc44ac341
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-17 20:10:00 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,20 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+		c++;
+	}
+	/*for (c = 'a'; c <= 'z'; c++)
 	{
 		if (c != 'e' && c != 'q')
 		{
 			putchar(c);
 		}
-	}
+		}*/
 	putchar('\n');
 	return (0);
 }
