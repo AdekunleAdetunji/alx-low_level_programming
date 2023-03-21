a026ac3b7b9ca588ad4fa427372b0a61af127721
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-21 04:49:01 -0700

    C functions, and nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all lowercase alphabets in 10 lines
+ * Return: returns a void value
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
