2d5adc0cd5f978a80a11a5ace62d7981ad417bbb
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-21 09:25:23 -0700

    C functions, and nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @i: The integer value that will be supplied to function
+ * Return: Returns null value
+ */
+void print_to_98(int i)
+{
+	if (i < 98)
+	{
+		for (; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+		putchar('\n');
+	}
+	else if (i > 98)
+	{
+		for (; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+		putchar('\n');
+	}
+}
