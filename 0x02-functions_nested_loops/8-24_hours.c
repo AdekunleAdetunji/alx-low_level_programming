8f341d8a5bf7146d2c986bdff6e7c5f663009ccb
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-21 07:48:56 -0700

    C functions, and nested loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute in a day
+ * Return - Returns void
+ */
+void jack_bauer(void)
+{
+	int m;
+	int s;
+
+	for (m = 0; m <= 23; m++)
+	{
+		for(s = 0; s <= 59; s++)
+		{
+			_putchar('0' + m/10);
+			_putchar('0' + m%10);
+			_putchar(':');
+			_putchar('0' + s/10);
+			_putchar('0' + s%10);
+			_putchar('\n');
+		}
+	}
+	return;
+}
