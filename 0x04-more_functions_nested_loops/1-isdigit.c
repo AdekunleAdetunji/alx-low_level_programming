e33d3f5c8cb591311a8c7e0bc58da1edb0b15434
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-23 11:18:53 -0700

    More c functions and loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - checks wether an input value is an integer
+ * @c: The argument function is working with
+ * Return: Returns 1 for an integer argument and 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 59)
+		return (1);
+	else
+		return (0);
+}
