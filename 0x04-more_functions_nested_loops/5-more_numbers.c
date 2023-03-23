fef866fc651e15295fef5bdcbcdac6a4be49ec12
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-23 12:06:21 -0700

    C more functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times 1-14
+ * Return: Returns a null value
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 14; x++)
+		{
+			if (x > 9)
+				_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
