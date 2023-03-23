023e699529b94c0b121b7b2fa893e1ea259f7797
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-23 11:25:51 -0700

    More c functions and loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+/**
+ * mul - returns the product of two input values
+ * @a: The first argument
+ * @b: The second argument
+ * Return: Returns the product of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
