2d4f1ed82dc694d9205a63e45117294f7a184adc
Adetunji <Adekunleadetunjiwilson@gmail.com>
2023-03-23 13:29:36 -0400

    More C functions and loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers between 0 and 9 except 2 and 4
+ * Return: Return null value
+ */
+void print_most_numbers(void)
+{
+	int c;
+	for (c = 0; c <= 9; c++)
+	{
+		if (c != 2 && c != 4)
+			_putchar('0' + c);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the values between 1-100, for values divisible by three
+ * the word fizz is printed, for those divisible by 5 buzz is returned
+ * and if it is divisible by both, fizzbuzz is printed.
+ * Return: Returns an integer value of zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 5 == 0 && n % 3 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+		putchar(' ');
+	}
+	return (0);
+}
