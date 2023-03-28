72c0a0a922fe9851081d39b0b4dfe40e4e988bab
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-28 07:33:17 -0700

    Programs demonstrating how to use C pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * count - counts the length of a string
+ * @s: The string add we want to count its value
+ *
+ * Return: Returns the count of the string contained in the address
+ */
+int count(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * puts2 - prints even integers in a set of string
+ * @str: Address of the array
+ *
+ * Return: Returns a null value
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = count(str);
+	for (i = 0; i <= len; i++)
+	{
+		if (*(str + i) % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
+}
