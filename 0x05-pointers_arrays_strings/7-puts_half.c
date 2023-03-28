7bb975668f0799f6bf44bde4866f9ee230570dc3
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-03-28 07:45:32 -0700

    Programs demonstrating how to use C pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * count - counts the length of a string
+ * @s: The string add we want to count its value
+ *
+ * Return: Returns the count of the string contained in the address
+ */
+int count(char *s)
+{
+	int i, count;
+
+	count = 0;
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * puts_half - prints the last half of a string followed by a new line
+ * @str: The address of the string we are considering
+ *
+ * Return: Returns a null value
+ */
+void puts_half(char *str)
+{
+	int i, len, half;
+
+	len = count(str);
+	half = len / 2;
+	for (i = half; i <= len; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\0');
+	_putchar('\n');
+}
