179fff95bf8f0d1b380b502fd17837e12fd44616
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-07 08:13:34 -0700

    Argv and Argc source files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -31,12 +31,12 @@ int main(int argc, char **argv)
 	int i, min, cent;
 
 	min = 0;
-	cent = atoi(argv[1]);
 	if (argc != 2 || checker(argv[1]))
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cent = atoi(argv[1]);
 	for (i = 0; cent > 0; i++)
 	{
 		if (cent / 25)
