c87d06c25db1afbd5b987f790fd5b2250d2c9a6c
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-07 07:54:01 -0700

    Argv and Argc source files

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * checker - checks if the input string is an integer
+ * @s: The input string address
+ * Return: Returns 1 if true and 0 if false
+ */
+int checker(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && s[i] == '+')
+			continue;
+		else if (!(s[i] >= '0' && s[i] <= '9'))
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * main - prints the minimum number of coins to make change for an amount of
  * money
  * @argv: The vector of the integer supplied via the command line
@@ -13,7 +32,7 @@ int main(int argc, char **argv)
 
 	min = 0;
 	cent = atoi(argv[1]);
-	if (argc > 2)
+	if (argc > 2 || checker(argv[1]))
 	{
 		printf("Error\n");
 		return (1);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * checker - checks if the input string is an integer
+ * @s: The input string address
+ * Return: Returns 1 if true and 0 if false
+ */
+int checker(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0 && (s[i] == '-' || s[i] == '+'))
+			continue;
+		else if (!(s[i] >= '0' && s[i] <= '9'))
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * main - prints the sum of integer arguments supplied via the command line
  * @argv: The vector holding the integer string arguments supplied via the CL
  * @argc: The count of arguments supplied to the CL
@@ -12,7 +31,7 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		if (!atoi(argv[i]) || checker(argv[i]))
 		{
 			printf("Error\n");
 			return (1);
