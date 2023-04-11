d94f0b2b950553cb6ff1454bf615c84f1084089c
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-11 11:25:41 -0700

    Malloc and free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int i, **mem_y, *mem_x;
 
 	if (width <= 0 || height <= 0)
-		return NULL;
+		return (NULL);
 
 	mem_y = malloc(sizeof(int *) * height);
 	if (!mem_y)
