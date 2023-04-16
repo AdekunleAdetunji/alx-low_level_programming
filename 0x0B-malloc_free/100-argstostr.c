d71d1f1e8194ef2a9d6deb81d17caa6521113723
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-16 11:38:05 -0700

    Malloc and free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates the argument supplied to main
+ * @ac: The count of argument supplied to main
+ * @av: The argument vector
+ * Return: A pointer or NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, count = 0;
+	char *mem;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		count += strlen(av[i]);
+
+	mem = malloc(sizeof(char) * count + ac);
+	if (!mem)
+		return (NULL);
+
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < (int)strlen(av[i]); j++)
+		{
+			mem[j + k] = av[i][j];
+		}
+		mem[j + k] = '\n';
+		k += j + 1;
+	}
+	return (mem);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * wc - counts the number of words in a string
+ * @s: The string we want to count its word
+ * Return: An integer value of the word count
+ */
+int wc(char *s)
+{
+	int i, count = 0, flag = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (flag == 1 && s[i] != ' ')
+		{
+			count += 1;
+			flag = 0;
+			continue;
+		}
+		else if (flag == 0 && s[i] == ' ')
+		{
+			flag = 1;
+			continue;
+		}
+	}
+	return (count);
+}
+
+/**
+ * strtow - breaks a string into words and stores this words in a new memory
+ * @str: The string we want to operate on
+ * Return: A pointer or Null value
+ */
+char **strtow(char *str)
+{
+	int i, x, y, count, word_len, ws, we;
+	char **mem_y, *mem_x;
+
+	count = wc(str);
+	mem_y = malloc(sizeof(char *) * (count + 1));
+	if (!mem_y)
+		return (NULL);
+	ws = 0;
+	we = 0;
+	for (y = 0; y < count; y++)
+	{
+		for (i = ws; str[i] != '\0'; i++)
+			if (str[i] != ' ')
+			{
+				ws = i;
+				break;
+			}
+		for (i = ws; str[i] != '\0'; i++)
+			if (str[i] == ' ')
+			{
+				we = i;
+				break;
+			}
+		word_len = we - ws + 1;
+		mem_x = malloc(sizeof(char) * word_len);
+		if (!mem_x)
+			return (NULL);
+		for (x = 0; x < word_len; x++)
+			mem_x[x] = str[x + ws];
+		ws = we;
+		mem_x[x] = '\0';
+		mem_y[y] = mem_x;
+	}
+	mem_y[y] = NULL;
+	return (mem_y);
+}
