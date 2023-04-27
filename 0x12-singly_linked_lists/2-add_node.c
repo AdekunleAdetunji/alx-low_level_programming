c38eea1242abd658d32396cd938018b0e679f9f8
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-27 12:53:47 -0700

    Removed the strlen function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,21 @@
 #include "lists.h"
 
 /**
+ * count - counts the number of character in a string
+ * @s: The string in consideration
+ * Return: Return the count of the string
+ */
+int count(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
+/**
  * add_node - Adds a new node at the beginning of an existing linked list
  * @head: The node under consideration
  * @str: The string that is to be contained in our node
@@ -19,7 +34,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	dupstr = strdup(str);
 	mem->str = dupstr;
-	mem->len = strlen(dupstr);
+	mem->len = count(dupstr);
 	mem->next = (*head);
 	(*head) = mem;
 
