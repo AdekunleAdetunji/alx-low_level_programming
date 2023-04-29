8dff5a0828941302753a627c53ea21891f251318
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-04-29 10:29:54 -0700

    Corrected betty error

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = mem;
 	else
 	{
-	        temp = *head;
+		temp = *head;
 		for (i = 0; temp->next != NULL; i++)
 		{
 			temp = temp->next;
