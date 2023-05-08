2d115a21b5ca748ec5840b8dc5998dca05ec11d0
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-05-08 07:38:15 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 
 	word_count = strlen(text_content);
 
-	fd = open(filename, O_CREAT | O_WRONLY, 0x600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0x600);
 	if (fd < 0)
 	{
 		close(fd);
