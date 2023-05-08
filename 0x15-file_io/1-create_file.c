151c5ed6b156a61c6f76d54501af2e0bb0f86a80
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-05-08 07:54:55 -0700

    Changed conditional

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,8 +28,8 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0x600);
-	if (fd < 0)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 	{
 		close(fd);
 		return (-1);
