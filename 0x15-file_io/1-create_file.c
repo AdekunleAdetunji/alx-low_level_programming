a78a6d50fa6db6b65a15a82a81fb3bbd9437ec36
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-05-08 07:50:58 -0700

    Added a new condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,8 +28,6 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	word_count = count(text_content);
-
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0x600);
 	if (fd < 0)
 	{
@@ -37,13 +35,16 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	bytes_written = write(fd, text_content, word_count);
-	if (bytes_written < 0)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		word_count = count(text_content);
+		bytes_written = write(fd, text_content, word_count);
+		if (bytes_written < 0)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
-
 	close(fd);
 	return (1);
 }
