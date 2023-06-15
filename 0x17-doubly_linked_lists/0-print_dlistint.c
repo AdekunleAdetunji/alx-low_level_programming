3b2cb0205f5b12fc7ec644329b8e536f2119188f
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 01:12:41 -0700

    solution to task 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints the nodes in a doubly linked list
+ * @h: The head node of the linked list
+ * Return: The size of the linked list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i;
+	const dlistint_t *temp;
+
+	temp = h;
+	for (i = 0; temp != NULL; i++)
+	{
+		printf("%u\n", temp->n);
+		temp = temp->next;
+	}
+
+	return (i);
+}
