d8d384b33205f679bef66f1665f1e70ec48a61ca
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 04:28:49 -0700

    Modified the loop

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,12 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
+	size_t i = 0;
 	const dlistint_t *temp;
 
 	temp = h;
-	for (i = 0; temp != NULL; i++)
+	while (temp)
 	{
+		i += 1;
 		printf("%u\n", temp->n);
 		temp = temp->next;
 	}
