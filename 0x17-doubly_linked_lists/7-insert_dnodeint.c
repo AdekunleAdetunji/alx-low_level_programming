da4d9771d66e970cbde73400862c260d0ee48df9
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 05:28:33 -0700

    Corrected betty error

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,8 +1,42 @@
 #include "lists.h"
 
 /**
+ * mini - mini function to carry out insertation in a linked list
+ * @idx: The index to insert at
+ * @h: The head node
+ * @new: The new node
+ * @temp: temporary node pointer
+ */
+void mini(unsigned int idx, dlistint_t *h, dlistint_t *new, dlistint_t *temp)
+{
+	dlistint_t *prev;
+
+	if (temp == NULL)
+	{
+		prev = h;
+		while (prev->next != NULL)
+			prev = prev->next;
+		prev->next = new;
+		new->prev = prev;
+	}
+	else
+	{
+		prev = temp->prev;
+		temp->prev = new;
+		new->prev = prev;
+		new->next = temp;
+
+		if (idx == 0)
+			h = new;
+		else
+			prev->next = new;
+	}
+
+}
+
+/**
  * insert_dnodeint_at_index - insert a node at a particular index
- * @head: The address of the head node of the linked list
+ * @h: The address of the head node of the linked list
  * @idx: The index where the new node is to be placed
  * @n: The value for the data field of the new node
  * Return: The address of the new node
@@ -10,7 +44,7 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
-	dlistint_t *temp, *prev, *new;
+	dlistint_t *temp, *new;
 
 	if (!h)
 		return (NULL);
@@ -34,27 +68,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-
-	if (temp == NULL)
-	{
-		prev = *h;
-		while (prev->next != NULL)
-			prev = prev->next;
-		prev->next = new;
-		new->prev = prev;
-	}
-	else
-	{
-		prev = temp->prev;
-		temp->prev = new;
-		new->prev = prev;
-		new->next = temp;
-
-		if (idx == 0)
-			*h = new;
-		else
-			prev->next = new;
-	}
+	mini(idx, *h, new, temp);
 
 	return (new);
 }
