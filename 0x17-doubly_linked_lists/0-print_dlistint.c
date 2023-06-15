b609a9dac5fc11780b368b4f8ad3d3f8c8ebb2e5
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 04:37:06 -0700

    Changed data type from %u to %d

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,11 +8,13 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
+	const dlistint_t *temp;
 
-	for (i = 0; h != NULL; i++)
+	temp = h;
+	for (i = 0; temp != NULL; i++)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 
 	return (i);
