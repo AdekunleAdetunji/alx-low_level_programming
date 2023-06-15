81230d1f8f56ee63ba733708d6ad2101eebaf35a
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 04:40:42 -0700

    Removed check for empty node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,9 +10,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *temp;
 
-	if (!head)
-		return (NULL);
-
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
