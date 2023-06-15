e960f4f3d16fe505ed6836541dca3262bc919878
Adetunji <adekunleadetunjiwilson@gmail.com>
2023-06-15 02:21:13 -0700

    solution to task 5

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees the memory allocated to doubly linked list
+ * @head: The head node of the doubly linked list
+ * Return: Void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	int i;
+	dlistint_t *temp, *temp_next;
+
+	temp = head;
+	for (i = 0; temp != NULL; i++)
+	{
+		temp_next = temp->next;
+		free(temp);
+		temp = temp_next;
+	}
+	free(temp);
+}
