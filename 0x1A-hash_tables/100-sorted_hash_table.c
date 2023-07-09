c22c7221f3828a4f66c88265f635dd38f01b13d1
adetunji <adekunleadetunjiwilson@gmail.com>
2023-07-09 13:07:47 -0500

    added comments to the functions

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -11,17 +11,17 @@ shash_table_t *shash_table_create(unsigned long int size)
 	shash_table_t *new;
 
 	new = malloc(sizeof(shash_table_t));
-	if (!new)
+	if (!new) /* check for failed memory allocation */
 		return (NULL);
 
 	new->size = size;
 	new->array = malloc(sizeof(shash_node_t *) * size);
-	if (!(new->array))
+	if (!(new->array)) /* check for failed hash_table_array memory allocation */
 	{
 		free(new);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++) /* allocate each array memory to NULL */
 		new->array[i] = NULL;
 
 	new->shead = NULL;
@@ -42,18 +42,22 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 	char *new_value, *new_key;
 	unsigned long int index;
 
+	/* ensure that key and table are valid */
 	if (!ht || !key || *key == '\0')
 		return (0);
 
 	new_value = strdup(value);
+	/* check for failed strdup */
 	if (!new_value)
 		return (0);
 	new_key = strdup(key);
+	/* check for failed strdup */
 	if (!new_key)
 	{
 		free(new_value);
 		return (0);
 	}
+	/* get key index */
 	index = key_index((const unsigned char *) new_key, ht->size);
 	insert_node(ht, new_key, new_value, index);
 
@@ -73,6 +77,7 @@ void insert_node(shash_table_t *ht, char *key, char *value,
 {
 	shash_node_t *new;
 
+	/* chech if array bucket is empty */
 	if (!ht->array[index])
 	{
 		new = malloc(sizeof(shash_node_t));
@@ -90,6 +95,7 @@ void insert_node(shash_table_t *ht, char *key, char *value,
 		ht->array[index] = new;
 		ord_insert(ht, new);
 	}
+	/* if not empty add new node handle collision */
 	else
 	{
 		han_col(ht, key, value, index);
@@ -112,8 +118,10 @@ void han_col(shash_table_t *ht, char *key, char *value,
 	shash_node_t *temp, *new;
 
 	temp = ht->array[index];
+	/* check if the key already exist in the array bucket */
 	while (temp && strcmp(temp->key, key) != 0)
 		temp = temp->next;
+	/* if it does exist update the node */
 	if (temp)
 	{
 		free(temp->value);
@@ -121,6 +129,7 @@ void han_col(shash_table_t *ht, char *key, char *value,
 		temp->value = value;
 		temp->key = key;
 	}
+	/* Add new node to the beginning of the arr_bucket */
 	else
 	{
 		new = malloc(sizeof(shash_node_t));
@@ -136,6 +145,7 @@ void han_col(shash_table_t *ht, char *key, char *value,
 		new->sprev = ht->stail;
 		new->snext = NULL;
 		ht->array[index] = new;
+		/* add the new node to the sorted hash table linked list */
 		ord_insert(ht, new);
 	}
 
@@ -153,11 +163,13 @@ void ord_insert(shash_table_t *ht, shash_node_t *new)
 
 	temp = ht->shead;
 	prev = NULL;
+	/* check if the sorted_linked list field is empty */
 	if (!(temp))
 	{
 		ht->shead = new;
 		ht->stail = new;
 	}
+	/* if it is not empty put the node in the apprioprate location */
 	else
 	{
 		while (temp)
@@ -178,6 +190,7 @@ void ord_insert(shash_table_t *ht, shash_node_t *new)
 			prev = temp;
 			temp = temp->snext;
 		}
+		/* place the new node at the end of the ht slinked list */
 		if (!temp)
 		{
 			ht->stail->snext = new;
@@ -200,15 +213,19 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 	unsigned long int index;
 	shash_node_t *head, *temp;
 
+	/* check for invalid key or ht */
 	if (!ht || !key || *key == '\0')
 		return (NULL);
 
+	/* get index of the key */
 	index = key_index((const unsigned char *) key, ht->size);
 
+	/* get the pointer to the index list */
 	head = ht->array[index];
 	if (!head)
 		return (NULL);
 
+	/* navigate the array bucket searching for a matching key */
 	temp = head;
 	while (temp)
 	{
@@ -217,8 +234,10 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 		temp = temp->next;
 	}
 
+	/* if there is no matching key return NULL */
 	if (!temp)
 		return (NULL);
+	/* return matching key */
 	return (temp->value);
 }
 
@@ -233,8 +252,10 @@ void shash_table_print(const shash_table_t *ht)
 
 	temp = ht->shead;
 	putchar('{');
+	/* navigate the ht sorted linked list using the header */
 	while (temp)
 	{
+		/* print the content of each node in the list */
 		printf("'%s': '%s'", temp->key, temp->value);
 		temp = temp->snext;
 		if (temp)
@@ -254,8 +275,10 @@ void shash_table_print_rev(const shash_table_t *ht)
 
 	temp = ht->stail;
 	putchar('{');
+	/* navigate the sorted linked list using the tail */
 	while (temp)
 	{
+		/* print the key and value of the current node */
 		printf("'%s': '%s'", temp->key, temp->value);
 		temp = temp->sprev;
 		if (temp)
@@ -266,6 +289,7 @@ void shash_table_print_rev(const shash_table_t *ht)
 
 /**
  * shash_table_delete - free the link allocated to a hash table
+ * using the shash_table shead field
  * @ht: The hashtable we are to free
  * Return: Null
  */
